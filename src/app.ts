c0e6c4b2997b475125df3dd673be0ac1a0257c07
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-03 01:25:26 -0300

    docs: adicionado documentação com swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import errorHandler from './middlewares/errorHandler';
 import routes from './routes';
+
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 app.use(express.json());
 
 routes(app);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
